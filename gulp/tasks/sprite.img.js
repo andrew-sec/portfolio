e0a55a05a02fdb97c9d19aea486457a1394e993d
Andrew S <sekachov@gmail>
2017-04-15 00:08:03 +0300

    add ttf and gif to tasks

--- gulp/tasks/sprite.img.js
+++ gulp/tasks/sprite.img.js
@@ -2,12 +2,17 @@
 
 module.exports = function() {
   
-  $.gulp.task('sprite:img', function () {
-  $.gp.spriteData = $.gulp.src('source/images/sprites/*.*').pipe($.gp.spritesmith({
+  $.gulp.task('sprite:img', function() {
+  $.gp.pngSpriteData = $.gulp.src('source/images/sprites/*.png').pipe($.gp.spritesmith({
     imgName: 'sprite.png',
     cssName: 'sprite.css'
   }));
-  return $.gp.spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img/')),
+  $.gp.gifSpriteData = $.gulp.src('source/images/sprites/*.gif').pipe($.gp.spritesmith({
+    imgName: 'sprite.gif',
+    cssName: 'sprite.css'
+  }));
+  return $.gp.pngSpriteData.img.pipe($.gulp.dest($.config.root + '/assets/img/')),
+         $.gp.gifSpriteData.img.pipe($.gulp.dest($.config.root + '/assets/img/')),
          $.gp.spriteData.css.pipe($.gulp.dest($.config.root + '/assets/css/'));
 });
 
