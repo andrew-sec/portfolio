a9be53289d13cee91373add680d83a089d70d33d
Andrew S <sekachov@gmail>
2017-04-19 14:44:07 +0300

    edit task copy.fonts

--- gulp/tasks/copy.fonts.js
+++ gulp/tasks/copy.fonts.js
@@ -3,8 +3,8 @@
 module.exports = function() {
 
   $.gulp.task('copy:fonts', function() {
-    return $.gulp.src('source/**/*.{woff,svg,ttf}')
-   .pipe($.gulp.dest($.config.root + '/assets/'));
+    return $.gulp.src('source/fonts/*.{woff,svg,ttf}')
+   .pipe($.gulp.dest($.config.root + '/assets/fonts'));
   });
 
 };
